1952004dbc8d122163ae32c63e4db112c40494b0
utkarshsystem <sharadgoyal@outlook.com>
2026-03-19 21:48:46 -0400

    pdf browser added

---
+++ vcp-s3-browser-ui/app/pdf/page.tsx
@@ -0,0 +1,14 @@
+
+import { PdfExplorerDashboard } from "../../src/components/PdfExplorer/PdfExplorerDashboard";
+
+export default function PdfPage() {
+	return (
+		<PdfExplorerDashboard
+			subscriberCode={"TA"}
+			role="admin"
+			elearningUrl={"https://elearning.virtusasystems.com/api"}
+			dashboardOnlyFlag={false}
+			dashboardCode={"tamathcount-ukmt"}
+		/>
+	);
+}
---
+++ vcp-s3-browser-ui/src/services/contact.js
@@ -0,0 +1,5 @@
+const getRolesApi = async (url) => {
+  const res = await fetch(`${url}/user/get-all-roles`);
+  return res;
+};
+export { getRolesApi };
